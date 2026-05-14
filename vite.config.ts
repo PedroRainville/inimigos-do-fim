d752cd61adda3193104c92eefc4d8cdbe9086b2b
Pedro Rainville <pedrorainville11@gmail.com>
2026-05-14 00:56:59 -0300

    change: package.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-	base: '/player-pool-pal/',
+	base: '/inimigos-do-fim/',
 	resolve: {
 		alias: {
 			"node:async_hooks": path.resolve(__dirname, "src/shims/node-async-hooks.js"),
